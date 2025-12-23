978e352a1e884cdf7e4278d2a47755fde09572c1
Ilya <ilya.biryuk2005@icloud.com>
2025-12-23 22:18:33 +0300

    first commit

---
+++ src/test/java/by/bsu/biryuk/Lab7/Lab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsu.biryuk.Lab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
